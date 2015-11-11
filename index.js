1eefc1920b400d1265926532ee15228aa4664d26
seemonz <seemonz@gmail.com>
2015-11-10 17:39:00 -0800

    removed modules from git, refactored code

--- index.js
+++ index.js
@@ -2,34 +2,35 @@ var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
 
-
 // gets the html
-request('http://substack.net/images/', function (error, response, body) {
+request('http://substack.net/images/', function(error, response, body) {
   if (!error && response.statusCode == 200) {
     getContent(body);
-  };
+  }
 });
 
-function getContent(body, callback) {
+function getContent(body) {
   $ = cheerio.load(body);
-  // console.log($('tr td'));
   var tables = $('body').find('tr');
-  var finalArr = [['file permission, absolute path, file type \n']];
-  tables.each(function(i, elem){
+  // seed arr with column headers
+  var finalArr = [
+    ['file permission, absolute path, file type \n']
+  ];
+  // grab each feild and push into arr, then push that array into finalArr
+  tables.each(function() {
     var arr = [];
     arr.push($(this).find('td code').html());
     arr.push('http://substack.net/' + $(this).find('a').html());
     arr.push($(this).find('a').html().split('.')[1] + '\n');
     finalArr.push(arr);
   });
+  // Write to file substack-imgs.csv
   var writeArr = [];
-  // console.log(finalArr);
   writeArr.push(finalArr.join(''));
-  fs.writeFile("paths.csv", writeArr, function(err) {
-      if(err) {
-          return console.log(err);
-      }
-
-      console.log("The file was saved!");
+  fs.writeFile('substack-imgs.csv', writeArr, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('The file was saved!');
   });
-};
+}
